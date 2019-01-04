848a4d75644c71fa00214edc0037585f11d943d4
Aisha-Sumaiya <aishasumaiya@gmail.com>
2019-01-04 23:15:13 +0600

    connection code runs perfectly

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+    //opens connection to mysql server
+    
+        $link=mysqli_connect("localhost","root","","hubbys_homework"); 
+        /*if(!$link){                        
+            echo mysql_error();
+        } 
+        else{
+            echo "Database connected"."</br>";
+        }*/
+
+?>
